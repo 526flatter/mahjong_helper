aff018283544e5868941b95980eabb148973c269
imoQ <526flatter@gmail.com>
2019-10-20 15:12:33 +0900

    牌セレクタ実装

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -1,21 +1,22 @@
 <template>
     <div>
         <div id="hand">
-            <img v-for="tile in hand" v-bind:key="tile" v-bind:src="tile_img['tile']">
+            <div v-for="(pai, idx) in noDrawingPai" :key="idx">
+                <img :src="getTileSrc(pai)"/>
+            </div>
+            <img :src="getTileSrc(drawingPai)" v-if="drawingPai"/>
         </div>
         <div id="buttonArea">
-            <button class="btn-border" v-on:click="select_tiles()">萬子</button>
-            <button class="btn-border">索子</button>
-            <button class="btn-border">筒子</button>
-            <button class="btn-border">字牌</button>
-            <div id="tiles">
-            </div>
+            <button class="btn-border" @click="openTileSelector('m')">萬子</button>
+            <button class="btn-border" @click="openTileSelector('s')">索子</button>
+            <button class="btn-border" @click="openTileSelector('p')">筒子</button>
+            <button class="btn-border" @click="openTileSelector('t')">字牌</button>
         </div>
-        <div id="tile_selector" v-show="tile_selector" v-on:click="unselect_tiles()">
-            <div>
-                <button>てすと</button>
+        <tile-selector v-if="showTileSelector" @close="closeTileSelector">
+            <div v-for="(pai, idx) in tiles[tileType]" :key="idx">
+                <img :src="getTileSrc(pai)" class="paiSelector" @click="addPai(pai)"/>
             </div>
-        </div>
+        </tile-selector>
         <div id="situation_box">
             <select v-model="situation.round">
                 <option v-for="round in situation_conditions.round"
@@ -64,18 +65,23 @@
 </template>
 
 <script>
+import TileSelector from '../components/TileSelector'
 
 export default {
   name: 'register',
+  components: {
+      'tile-selector': TileSelector
+  },
   data(){
       return {
           hand: [],
-          tile_selector: false,
-          tile: {
-
-          },
-          tile_img: {
-
+          showTileSelector: false,
+          tileType: "m",
+          tiles: {
+              'm': ['m1', 'm2', 'm3', 'm4', 'm5', 'm6', 'm7', 'm8', 'm9', 'm0'],
+              's': ['s1', 's2', 's3', 's4', 's5', 's6', 's7', 's8', 's9', 's0'],
+              'p': ['p1', 'p2', 'p3', 'p4', 'p5', 'p6', 'p7', 'p8', 'p9', 'p0'],
+              't': ['t1', 't2', 't3', 't4', 't5', 't6', 't7']
           },
           situation:  {
               round: '東', // 場風
@@ -96,12 +102,55 @@ export default {
           note: ""
       }
   },
-  methods: {
-      select_tiles: function(){
-          this.tile_selector = true
+  computed: {
+      tileSrc2: function(){
+          const srcTemplate = './img/pai-images/'
+          const tileSerial = ['1', '2', '3', '4', '5', '6', '7']
+          const ext = '.png'
+
+          let tileSrc = tileSerial.map(serial => srcTemplate + this.tileType + serial + ext)
+
+          if(this.tileType !== 't'){
+            //   tileSrc.splice(5, srcTemplate + this.tileType + '0' + ext) // 赤5
+              tileSrc.push(srcTemplate + this.tileType + '8' + ext) // 8と9 字牌は7種なので
+              tileSrc.push(srcTemplate + this.tileType + '9' + ext)
+              tileSrc.push(srcTemplate + this.tileType + '0' + ext) // 赤5
+          }
+          console.log(tileSrc)
+          return tileSrc
       },
-      unselect_tiles: function() {
-          this.tile_selector = false
+      noDrawingPai: function(){
+          if (this.hand.length < 14){
+              return this.hand
+          } else {
+              return this.hand.slice(0, 13)
+          }
+      },
+      drawingPai: function(){
+          if (this.hand.length < 14){
+              return ""
+          } else {
+              return this.hand[13]
+          }
+      }
+  },
+  methods: {
+      openTileSelector: function(tileType){
+          this.tileType = tileType
+          this.showTileSelector = true
+      },
+      closeTileSelector: function(){
+          this.showTileSelector = false
+      },
+      getTileSrc: (pai) => {
+          const srcTemplate = './img/pai-images/'
+          return `${srcTemplate}${pai}.png`
+      },
+      addPai: function(pai){
+          if (this.hand.length === 14){
+              return
+          }
+          this.hand.push(pai)
       }
   }
 }
@@ -119,6 +168,7 @@ export default {
     border-color: #41b879;
     border-width: 2px;
     border-radius: 8px;
+    display: flex;
 }
 #buttonArea {
     margin-top: 10px;
@@ -144,6 +194,9 @@ export default {
     opacity: 0.20;
     position: fixed;
     display: block;
+}
+.paiSelector {
+    margin: 10px;
 }
 #situation_box {
     display: flex;
